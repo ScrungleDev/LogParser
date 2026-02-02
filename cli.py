7e9dc0e932ce5767e4e398e8484008b8fc7dc88a
ScrungleDev <dustinsmith514@gmail.com>
2026-02-02 13:25:52 -0500

    implemented cli skeleton

---
+++ cli.py
@@ -0,0 +1,10 @@
+import argparse
+
+def main():
+    parser =argparse.ArgumentParser(
+        description="LogParser - A modular log parsing and analysis tool"
+    )
+    parser.parse_args()
+
+if __name__ == "__main__":
+    main()
